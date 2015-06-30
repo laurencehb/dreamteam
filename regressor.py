a7242e7c8a28fe02a44875ec517ddf3db327bcb0
Balazs Kegl <balazs.kegl@gmail.com>
2015-06-30 20:51:19 +0200

    s2

--- regressor.py
+++ regressor.py
@@ -1,9 +1,9 @@
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.base import BaseEstimator
 
 class Regressor(BaseEstimator):
     def __init__(self):
-        self.clf = LinearRegression()
+        self.clf = RandomForestRegressor(n_estimators=10, max_depth=10, max_features=10)
 
     def fit(self, X, y):
         self.clf.fit(X, y)
