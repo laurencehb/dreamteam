c3e03b50836d4589c195588d424fe7249b9f5ddc
unknown <xcallens@NCEL22688.iis.amadeus.net>
2015-07-04 09:44:21 +0200

    AdaBoost without feature selection and hyperparameter tuning

--- regressor.py
+++ regressor.py
@@ -1,12 +1,15 @@
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.base import BaseEstimator
+from sklearn.ensemble import AdaBoostRegressor
 
 class Regressor(BaseEstimator):
     def __init__(self):
-        self.clf = RandomForestRegressor(n_estimators=501, max_depth=5, max_features=12)
+        cl = RandomForestRegressor(n_estimators=10, max_depth=10, max_features=10)
+        self.clf = AdaBoostRegressor(base_estimator = cl, n_estimators=100)
 
     def fit(self, X, y):
         self.clf.fit(X, y)
 
     def predict(self, X):
         return self.clf.predict(X)
+#RandomForestClassifier
